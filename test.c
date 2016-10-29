f752b953cb45bd2c91b7e4e1cc03241b140b0931
goyyapazham <nalanda99@gmail.com>
2016-10-29 11:04:33 -0400

    finishedgit status

--- test.c
+++ test.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <time.h>
 
 int main() {
 
   struct stat *buf;
   stat("test.txt", buf);
-  printf("file size: %d\n", buf->st_size);
+  printf("file size: %lld\n", buf->st_size);
   printf("permissions: %o\n", buf->st_mode);
+
+  struct tm *info;
+  time_t rawtime = buf->st_atime;
+
+  info = localtime( &rawtime );
+
+  char buffer [80];
+
+  strftime(buffer,80,"time of last access: %c", info);
+  puts(buffer);
 
   return 0;
 }
