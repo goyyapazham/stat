4d62d2443d5c718094a4f82993032ad5277a6afe
goyyapazham <nalanda99@gmail.com>
2016-10-31 15:37:18 -0400

    alskdjalsdj

--- makefile
+++ makefile
@@ -1,6 +1,8 @@
 test: test.c
 	gcc test.c -o test
+
 clean:
 	rm *~
+
 run: test
 	./test
--- test.c
+++ test.c
@@ -2,15 +2,60 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <math.h>
+#include <string.h>
+
+char * ret_perm(mode_t dig) {
+  char *buff = (char *buff)malloc(3);
+  if( dig == 0 )
+    strcpy(buff, "---");
+  if( dig == 1 )
+    strcpy(buff, "--x");
+  if( dig == 2 )
+    strcpy(buff, "-w-");
+  if( dig == 3 )
+    strcpy(buff, "-wx");
+  if( dig == 4 )
+    strcpy(buff, "r--");
+  if( dig == 5 )
+    strcpy(buff, "r-x");
+  if( dig == 6 )
+    strcpy(buff, "rw-");
+  if( dig == 7 )
+    strcpy(buff, "rwx");
+  char ret[3] = *buff;
+  free(buff);
+  return ret;
+}
 
 int main() {
 
-  struct stat *buf;
+  //create stat buffer
+  struct stat *buf = (struct stat *)malloc(sizeof(struct stat));
   stat("test.txt", buf);
-  
-  printf("file size: %lld\n", buf->st_size);
-  printf("permissions: %o\n", buf->st_mode);
 
+  //FILE SIZE
+  float size = (float)(buf->st_size);
+  if(size > 1024000000)
+    printf("file size: %.1f %s\n", size / 1000000000, "GB");
+  else if(size > 1024000)
+    printf("file size: %.1f %s\n", size / 1000000, "MB");
+  else if(size > 1024)
+    printf("file size: %.1f %s\n", size / 1000, "KB");
+  else
+    printf("file size: %d %s\n", (int)size, "B");
+
+  //PERMISSIONS
+  mode_t mode = (buf->st_mode) % 512;
+  char first[3];
+  strcpy(first, ret_perm(mode / 64));
+  char second[3];
+  strcpy(second, ret_perm((mode / 8) % 8));
+  char third[3];
+  strcpy(third, ret_perm(mode % 8)); 
+  printf("permissions: %s %s %s\n", first, second, third);
+
+  //TIME OF LAST ACCESS
   struct tm *info;
   time_t rawtime = buf->st_atime;
 
